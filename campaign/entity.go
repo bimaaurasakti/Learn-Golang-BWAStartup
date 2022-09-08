f060c9bd0be14dc6055e8107cc6aa3e8c68e509b
Bima Aura <bimaaurasakti2001@gmail.com>
2022-09-08 22:44:35 +0700

    create entity and repository campaign

--- campaign/entity.go
+++ campaign/entity.go
@@ -1,1 +1,33 @@
 package campaign
+
+import "time"
+
+type Campaign struct {
+	ID             	 int
+	UserID         	 int
+	Name           	 string
+	ShortDescription string
+	Description      string
+	GoalAmount   	 string
+	CurrentAmount 	 string
+	Perks            string
+	BackerCount      string
+	Slug         	 string
+	CreatedAt      	 time.Time
+	UpdatedAt 	   	 time.Time
+}
+
+type CampaignImage struct {
+	ID             	 int
+	UserID         	 int
+	Name           	 string
+	ShortDescription string
+	Description      string
+	GoalAmount   	 string
+	CurrentAmount 	 string
+	Perks            string
+	BackerCount      string
+	Slug         	 string
+	CreatedAt      	 time.Time
+	UpdatedAt 	   	 time.Time
+}
--- campaign/repository.go
+++ campaign/repository.go
@@ -1,1 +1,40 @@
 package campaign
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	GetAll() ([]Campaign, error)
+	GetByUserID(userID int) ([]Campaign, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) GetAll() ([]Campaign, error) {
+	var campaign []Campaign
+
+	err := r.db.Find(&campaign).Error
+
+	if err != nil {
+		return campaign, err
+	}
+
+	return campaign, nil
+}
+
+func (r *repository) GetByUserID(userID int) ([]Campaign, error) {
+	var campaign []Campaign
+
+	err := r.db.Where("user_id = ?", userID).Find(&campaign).Error
+
+	if err != nil {
+		return campaign, err
+	}
+
+	return campaign, nil
+}
